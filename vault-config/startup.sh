425a797ea1146877ab108a39409e29e4c291b9fe
bbarkhouse-hashicorp <84409718+bbarkhouse-hashicorp@users.noreply.github.com>
2024-01-29 13:57:40 -0500

    comment cleanup

--- vault-config/startup.sh
+++ vault-config/startup.sh
@@ -20,8 +20,4 @@ while true
         fi
     done
 vault status
-apk update && apk add ca-certificates
-cp /vault/config/tfe-ca.crt /usr/local/share/ca-certificates/tfe-ca.crt
-chmod 644 /usr/local/share/ca-certificates/tfe-ca.crt
-update-ca-certificates
 tail -f vault.log
